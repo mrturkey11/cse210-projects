1ec671e28a5fcb0b82fe821aa215838d71a2f31b
Aaron D <aaronmdefriez@gmail.com>
2025-09-18 14:23:28 -0600

    Finished prepwork 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine(); 
+        int percentage = int.Parse(input);
+        char letterGrade;
+        if (percentage >= 90)
+            letterGrade = 'A';
+        else if (percentage >= 80)
+            letterGrade = 'B';
+        else if (percentage >= 70)
+            letterGrade = 'C';
+        else if (percentage >= 60)
+            letterGrade = 'D';
+        else
+            letterGrade = 'F';
+
+        int secondDigit = int.Parse(input.Substring(1));
+        string modifier = "";
+        if (secondDigit >= 7)
+            modifier = "+";
+        else if (secondDigit <= 3)
+            modifier = "-";
+        bool exeptions = (modifier == "+" && letterGrade == 'A') || letterGrade == 'F';
+
+        bool an = letterGrade == 'A' || letterGrade == 'F';
+        string grammer = an ? "an" : "a";
+
+        string message = percentage >= 70 ? "Congradulations you pass!" : "Mission failed we'll get 'em next time";
+        Console.WriteLine($"Your letter grade is {grammer} {letterGrade}{(exeptions ? "" : modifier)}");
+        Console.Write(message);
     }
 }
