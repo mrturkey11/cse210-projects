f3f657e9993c6323e4280c4b6d62c662c10bf95b
Aaron D <aaronmdefriez@gmail.com>
2025-09-25 10:43:56 -0600

    Finished Prep4-5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number == 0)
+                    break;
+                numbers.Add(number);
+            }
+            catch
+            {
+                Console.WriteLine("Enter a valid number");
+            }
+        }
+        int sum = 0;
+        int largestNumber = -9999;
+        int lowestPositive = 9999;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+            if (numbers[i] > largestNumber)
+                largestNumber = numbers[i];
+            if (numbers[i] < lowestPositive && numbers[i] > 0)
+                lowestPositive = numbers[i];
+        }
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + sum / numbers.Count);
+        Console.WriteLine("The highest number is: " + largestNumber);
+        Console.WriteLine("The smallest positive number is: " + lowestPositive);
+        Console.WriteLine("The sorted list is: ");
+        numbers.Sort();
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        int favoriteNumber = 3;
+        Console.Write("Please enter your favorite number: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+            favoriteNumber = number;
+        else
+            Console.WriteLine("Since you have refused to enter a valid number your favorite number is 3");
+
+        Console.Write("Please enter the year you were born: ");
+        int birthdate = 1984;
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int dob))
+            {
+                birthdate = dob;
+                break;
+            }
+            else
+                Console.WriteLine("Enter a valid date");
+        }
+        Console.WriteLine($"{name}, the square of your number is {favoriteNumber * favoriteNumber}");
+        Console.WriteLine($"{name}, you will turn {System.DateTime.Today.Year - birthdate} this year");
     }
 }
